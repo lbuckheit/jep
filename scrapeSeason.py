a4ff0888135ef683b9736a4a20afdd384d1a8b4a
Luke Buckheit <lukebuckheit@gmail.com>
2020-11-15 21:13:29 -0500

    updated comments

--- scrapeSeason.py
+++ scrapeSeason.py
@@ -1,5 +1,3 @@
-# TODO - Here's where the loop that scrapes each game in sequence will live
-# TODO - Perhaps scraping on a season-by-season basis is the best way to break it out
 import time
 from getSeasonGames import getSeasonGames
 from gameScraper import scrapeGame
