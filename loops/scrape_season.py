67fea885777cb7463617e9b6c0c9337a0909569e
Luke Buckheit <lukebuckheit@gmail.com>
2020-11-22 19:54:12 -0500

    more reorganization

--- loops/scrape_season.py
+++ loops/scrape_season.py
@@ -1,6 +1,6 @@
 import time
-from get_season_games import get_season_games
-from scrape_game import scrape_game
+from raw_scrapers.get_season_games import get_season_games
+from raw_scrapers.scrape_game import scrape_game
 
 # Goal is to get seasons 14-Present (J! Archive starts to miss 20+ episodes per season from 1-13, so I think those are less helpful and they're also so old that they're not as relevant)
 # Seasons scraped: 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 36, GOAT
